68c1832ca55569a5ab61f8f466b9d71bba6abbdc
khushboodudeja <147654319+khushboodudeja@users.noreply.github.com>
2025-04-05 13:47:01 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,26 @@
+// data.js
+const products = [
+    {
+      id: 1,
+      name: "Noise Smartwatch",
+      price: 1999,
+      image: "https://m.media-amazon.com/images/I/61YgN6ry3BL._SX679_.jpg",
+    },
+    {
+      id: 2,
+      name: "Zebronics Mouse",
+      price: 699,
+      image: "https://m.media-amazon.com/images/I/61Vt3kroLCL._SX679_.jpg",
+    },
+    {
+      id: 3,
+      name: "ASUS ROG Laptop",
+      price: 58999,
+      image: "https://m.media-amazon.com/images/I/71m6g5fHhJL._SX679_.jpg",
+    }
+  ];
+  
+  let cart = [];
+  
+  module.exports = { products, cart };
+  
